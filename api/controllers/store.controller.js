4ccac863329c23d6ad8f8806a87fea9d903709ef
Abdullah Emad <abdullahem1997@hotmail.com>
2018-03-28 14:47:14 +0200

    Create store controller:
    Created the store controller and the stubs for the exported function that
    will be used for the routes

---
+++ api/controllers/store.controller.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose'),
+  moment = require('moment'),
+  Validations = require('../utils/validations'),
+  User = mongoose.model('User');
+
+
+
+
+module.exports.createItems = function(req, res, next) {
+
+
+}
+
+
+module.exports.viewItems = function(req, res, next) {
+
+
+}
+
+
+
+module.exports.editItems = function(req, res, next) {
+
+
+}
+
+
+
+module.exports.deleteItems = function(req, res, next) {
+
+
+}
+
+
+module.exports.buyItems = function(req, res, next) {
+
+
+}
+
+
+module.exports.likeItems = function(req, res, next) {
+
+
+}
+
+
+
