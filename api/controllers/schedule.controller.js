71b0f713910325c663863705b63a4fcf92c06343
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2018-03-29 19:03:52 +0200

    added createTeacherSchedule method

--- api/controllers/schedule.controller.js
+++ api/controllers/schedule.controller.js
@@ -1,2 +1,44 @@
 var mongoose = require('mongoose'),
-    User = mongoose.model('User');
+    Teacher = mongoose.model('User');
+
+
+module.exports.createTeacherSchedule = function(req, res, next) {
+    /* var valid = req.params._id && Validations.isString(req.params._id);
+     if (!valid) {
+         return res.status(422).json({
+             err: null,
+             msg: 'name(String) is a required field.',
+             data: null
+         });
+     }*/
+    //console.log(req.params.email);
+    Teacher.findById(req.params.UserId).exec(function(err, user) {
+
+        console.log(req.params.UserId);
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res
+                .status(404)
+                .json({ err: null, msg: 'User not found.', data: null });
+        }
+        if(user.role !='Teacher'){
+            return res
+                .status(401)
+                .json({ err: null, msg: 'Unauthorized Access.', data: null });
+        }
+
+        user.schedule.Timetable = [[]];
+        user.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.status(201).json({
+                err: null,
+                msg: 'Schedule created successfully.',
+                data: user.schedule.Timetable
+            });
+        });
+    });
+};
