8c23e6ba40fed44476a08febd0f918489717620a
Abdullah Kady <abdullahkady@gmail.com>
2018-03-30 08:41:12 +0200

    Refined authentication, added child registerations (by parent)

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,48 +1,14 @@
 var express = require('express'),
   router = express.Router(),
   jwt = require('jsonwebtoken'),
-  authCtrl = require('../controllers/auth.controller');
-
-var isAuthenticated = function(req, res, next) {
-  // Check that the request has the JWT in the authorization header
-  var token = req.headers['authorization'];
-  if (!token) {
-    return res.status(401).json({
-      error: null,
-      msg: 'You have to login first!',
-      data: null
-    });
-  }
-  // Verify that the JWT is created using our server secret and that it hasn't expired yet
-  jwt.verify(token, req.app.get('secret'), function(err, decodedToken) {
-    if (err) {
-      return res.status(401).json({
-        error: err,
-        msg: 'Login timed out, please login again.',
-        data: null
-      });
-    }
-    req.decodedToken = decodedToken;
-    next();
-  });
-};
-
-var isNotAuthenticated = function(req, res, next) {
-  // Check that the request doesn't have the JWT in the authorization header
-  var token = req.headers['authorization'];
-  if (token) {
-    return res.status(403).json({
-      error: null,
-      msg: 'You are already logged in.',
-      data: null
-    });
-  }
-  next();
-};
+  authCtrl = require('../controllers/auth.controller'),
+  mw = require('./middlewares');
 
 //---------------------------- Authentication Routes --------------------------------//
-router.post('/auth/register', isNotAuthenticated, authCtrl.register);
-router.post('/auth/login', isNotAuthenticated, authCtrl.login);
+router.post('/auth/register', mw.isNotAuthenticated, authCtrl.register);
+router.post('/auth/login', mw.isNotAuthenticated, authCtrl.login);
+router.post('/auth/child', mw.isAuthenticated, mw.isNotChild, authCtrl.addChild);
 //----------------------------------------------------------------------------------//
+
 
 module.exports = router;
