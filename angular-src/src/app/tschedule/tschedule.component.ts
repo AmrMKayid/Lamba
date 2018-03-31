c8c2fcfdaf1d61643bcdd45b68cef8c7c06b17d5
Mohamed Helal <m.h.helal1997@gmail.com>
2018-03-31 22:19:21 +0200

    fixing front

--- angular-src/src/app/tschedule/tschedule.component.ts
+++ angular-src/src/app/tschedule/tschedule.component.ts
@@ -13,19 +13,23 @@ export class TscheduleComponent implements OnInit {
   public timetable;
   public day = [];
 
-  constructor(private http: HttpClient,) {
+  constructor(private http: HttpClient) {
 
   }
 
   createTeacherSchedule(){
     //let user= JSON.parse(localStorage.getItem('currentUser')).user;
-    this.http.post('http://localhost:3000/api/schedule/createTeacherSchedule/5abf8255adae82c5227d1c74',null ).subscribe();
+    this.http.post('http://localhost:3000/api/schedule/createTeacherSchedule/5abfe4bfadae82c5227d1c75',null ).subscribe();
   }
 
   getTeacherSchedule(){
     //let user= JSON.parse(localStorage.getItem('currentUser')).user;
-    this.http.get('http://localhost:3000/api/schedule/getTeacherSchedule/5abf8255adae82c5227d1c74').subscribe((res: any) => { this.timetable = res.data; });
-     this.day = this.timetable.saturday;
+    this.http.get('http://localhost:3000/api/schedule/getTeacherSchedule/5abfe4bfadae82c5227d1c75').subscribe((res: any) => { this.timetable = res.data.table;
+      console.log(res.data.table);
+      this.day = this.timetable.saturday;
+    });
+   console.log(this.timetable);
+    this.day = this.timetable.saturday;
 
   }
 
