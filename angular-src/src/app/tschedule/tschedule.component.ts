952e7345acd8c74591038c0845477556fcf0f411
Mohamed Helal <m.h.helal1997@gmail.com>
2018-04-01 00:20:56 +0200

    trying to create tabs

--- angular-src/src/app/tschedule/tschedule.component.ts
+++ angular-src/src/app/tschedule/tschedule.component.ts
@@ -12,6 +12,13 @@ export class TscheduleComponent implements OnInit {
 
   public timetable;
   public day = [];
+  public sat = [];
+  public sun = [];
+  public mon = [];
+  public tues = [];
+  public wed = [];
+  public thurs = [];
+  public fri = [];
 
   constructor(private http: HttpClient) {
 
@@ -19,19 +26,50 @@ export class TscheduleComponent implements OnInit {
 
   createTeacherSchedule(){
     //let user= JSON.parse(localStorage.getItem('currentUser')).user;
-    this.http.post('http://localhost:3000/api/schedule/createTeacherSchedule/5abfe6f3750ffc19b0689a36',null ).subscribe();
+    this.http.post('http://localhost:3000/api/schedule/createTeacherSchedule/5abfef1aadae82c5227d1c76',null ).subscribe();
   }
 
   getTeacherSchedule(){
     //let user= JSON.parse(localStorage.getItem('currentUser')).user;
-    this.http.get('http://localhost:3000/api/schedule/getTeacherSchedule/5abfe6f3750ffc19b0689a36').subscribe((res: any) => { this.timetable = res.data.table;
-      this.day = this.timetable.saturday;
-      console.log(this.day);
+    this.http.get('http://localhost:3000/api/schedule/getTeacherSchedule/5abfef1aadae82c5227d1c76').subscribe((res: any) => {
+      this.sat = res.data.table.saturday;
+      this.sun = res.data.table.sunday;
+      this.mon = res.data.table.monday;
+      this.tues = res.data.table.tuesday;
+      this.wed = res.data.table.wednesday;
+      this.thurs = res.data.table.thursday;
+      this.fri = res.data.table.friday;
     });
 
   }
 
+
+
   ngOnInit() {
+    this.getTeacherSchedule();
   }
+
+   fsat() {
+    this.day = this.sat;
+  }
+   fsun() {
+    this.day = this.sun;
+  }
+   fmon() {
+    this.day = this.mon;
+  }
+   ftues() {
+    this.day = this.tues;
+  }
+   fwed() {
+    this.day = this.wed;
+  }
+  fthurs() {
+    this.day = this.thurs;
+  }
+   ffri() {
+    this.day = this.fri;
+  }
+
 
 }
