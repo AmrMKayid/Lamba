3be391707308e4b27f25df42e4e530c24f4b8a2a
Amr M. Kayid <amrkayid2027@gmail.com>
2018-03-31 20:16:07 +0200

    Create Profile Component & SubComponents

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AdminComponent } from './components/profile/admin/admin.component';
+import { ParentComponent } from './components/profile/parent/parent.component';
+import { TeacherComponent } from './components/profile/teacher/teacher.component';
+import { ChildComponent } from './components/profile/child/child.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AdminComponent,
+    ParentComponent,
+    TeacherComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
