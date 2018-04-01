ed368328490239b4367326963d85f4478deabf6d
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-02 00:38:43 +0200

    Added Parent Add Child

--- angular-src/src/app/components/profile/parent/parent.component.ts
+++ angular-src/src/app/components/profile/parent/parent.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
+import {AuthService} from '../../../services/auth.service';
+import {ToasterService} from "angular5-toaster";
+import {appConfig} from "../../../app.config";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +15,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  childFirstName;
+  childlastName;
+  childUsername;
+  childPassword;
+  childConfirmPassword;
+  childGender;
+  newChildBtn: boolean;
+
+  constructor(private router: Router,
+              private http: HttpClient,
+              private toaster: ToasterService) {
+  }
 
   ngOnInit() {
+
+    this.newChildBtn = false;
+  }
+
+  newChild() {
+
+    let newChild = {
+      name: {
+        firstName: this.childFirstName,
+        lastName: this.childlastName
+      },
+      username: this.childUsername,
+      password: this.childPassword,
+      confirmPassword: this.childConfirmPassword,
+      gender: this.childGender,
+    };
+
+    this.http.post(appConfig.apiUrl + '/auth/child', newChild).subscribe(
+      data => {
+        this.toaster.pop({
+          type: 'success',
+          title: "Success!",
+          body: "You've been successfully created New Child Account!",
+          timeout: 3000
+        });
+      },
+      error => {
+        this.toaster.pop({
+          type: 'error',
+          title: "Error!",
+          body: error.msg,
+          timeout: 3000
+        });
+      });
+
   }
 
 }
