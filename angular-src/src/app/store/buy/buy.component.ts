3247e6b8eaf2a9c48c0cc093ecb80f9360b904e6
MayarAlfares <35994808+MayarAlfares@users.noreply.github.com>
2018-04-02 19:43:28 +0200

    Update buy.component.ts

--- angular-src/src/app/store/buy/buy.component.ts
+++ angular-src/src/app/store/buy/buy.component.ts
@@ -1,15 +1,48 @@
+import { Http, Headers, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import {ToasterContainerComponent, ToasterService} from 'angular5-toaster';
+import { StoreService } from '../../../services/store.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import {Observable} from 'rxjs/Rx';
 
-@Component({
-  selector: 'app-buy',
-  templateUrl: './buy.component.html',
-  styleUrls: ['./buy.component.css']
-})
-export class BuyComponent implements OnInit {
+Component({
+    selector: 'app-buy',
+    templateUrl: './buy.component.html',
+    styleUrls: ['./buy.component.css']
+  })
+  export class BuyComponent  {
+  
+    constructor( private toaster: ToasterService,
+    private http : Http,
+    private storeservice : StoreService,
+    private router : Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+     buyerId : string ;
+     itemId : string ;
+    buy() {
+  
+      this.buyerId = "Test";
+      this.itemId = "Test";
+      
+          let updatedProduct = {
+            name: this.buyerId,
+            itemId: this.itemId
+          };
+      
+          this.http.patch('http://localhost:3000/api/store/buy/:itemId' + this.itemId , updatedProduct)
+            .catch(err => {
+              return Observable.throw(err)
+            })
+            .subscribe(res => {
+            });
+      
+      
+   
+      
+        }
+      
+  
   }
-
-}
+  
