3f6135dd6cdd408c5fe8f64d9e65d9fb07acf955
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-06 12:24:14 +0200

    Create multiple items insertions test

--- test/store.js
+++ test/store.js
@@ -8,7 +8,7 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app');
 let should = chai.should();
-
+let assert = chai.assert;
 chai.use(chaiHttp);
 
 const user = {
@@ -62,6 +62,10 @@ function CreateItemsTests()
 {
 	describe('Create Items', function(){
 		it('It should create a new Item',createItemCat);
+	});
+
+	describe('Create multiple Items', function(){
+		it('It should create multiple Items',createMultipleItems);
 	});
 }
 
@@ -139,7 +143,10 @@ function ViewItemsTests()
  *                                                  									 *
  *****************************************************************************************/
 
-
+ /** done -> void
+   * Sends a post request with an item and checks if the server creates this item successfully
+   * Function done: should be called when the test is done
+   */
  function createItemCat(done)
  {
 	const item = {
@@ -154,10 +161,98 @@ function ViewItemsTests()
 
  	chai.request(server).post('/api/store/create').set('authorization', auth_token).send(item)
  	.end((err, res) => {
+
+ 		// checks for the formate of the response
  		res.body.should.have.property('msg').eql('Created Item successfully');
  		res.body.should.have.property('data').should.be.a('object');
+
+ 		// checks that the item was actually inserted in the database
  		Item.find({}, function (err, docs) {
+ 	
+ 			docs.should.have.property('length').eql(1);
+ 			
+
+ 			actual_item = docs[0];
+ 			try
+ 			{
+ 		 		assertItem(item, actual_item);
+ 		 	}
+ 		 	catch(err)
+ 		 	{
+ 		 		console.log(err);
+ 		 	}
  			done();
 		});
  	});
+ }
+
+ function createMultipleItems()
+ {
+ 	const item1 = {
+		name: 'cats',
+		description: 'A cat that has 4 legs and 2 eyes',
+		quantity: 3,
+		price: 300000,
+		item_type: 'pet',
+		item_condition: 'bad',
+		picture_url: 'img-123213'
+	}
+ 	const item2 = {
+	name: 'dogs',
+	description: 'A dog that has 4 legs and 2 eyes',
+	quantity: 3,
+	price: 300000,
+	item_type: 'pet',
+	item_condition: 'bad',
+	picture_url: 'img-123213'
+	}
+ 	const item3 = {
+	name: 'bolbol',
+	description: 'A toy',
+	quantity: 3,
+	price: 300000,
+	item_type: 'toy',
+	item_condition: 'good',
+	picture_url: 'img-123213'
+	}
+
+
+	chai.request(server).post('/api/store/create').set('authorization', auth_token).send(item1)
+ 	.end((err, res) => {
+ 		chai.request(server).post('/api/store/create').set('authorization', auth_token).send(item2)
+ 		.end((err, res) => {
+ 			chai.request(server).post('/api/store/create').set('authorization', auth_token).send(item)
+ 			.end((err, res) => {
+ 				Item.find({}, function (err, docs) {
+ 				console.log(docs.length);
+	 			docs.should.have.property('length').eql(3);
+	 			
+
+	 			actual_item = docs[0];
+	 		 	assertItem(item1, actual_item);
+	 		 	actual_item = docs[1];
+	 		 	assertItem(item2, actual_item);
+	 		 	actual_item = docs[2];
+	 		 	assertItem(item3, actual_item);
+	 			done();
+			});	
+ 			});
+ 		});
+ 	});
+
+ }
+
+/** item, item -> void
+  * Asserts the expected item against the actual item
+  * Item expected: the exepected item
+  * Item actual: the actual item that was inserted to the database
+  */
+ function assertItem(expected, actual)
+ {
+	// makes sure the correct fields were inserted
+	actual.should.have.property('name').eql(expected.name);
+	actual.should.have.property('description').eql(expected.description);
+	actual.should.have.property('price').eql(expected.price);
+	actual.should.have.property('item_type').eql(expected.item_type);
+	actual.should.have.property('item_condition').eql(expected.item_condition);
  }
