93b5530fa572854e30a94187084e119c4ba7fbd7
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2018-04-06 14:55:32 +0200

    added some pagination css

--- angular-src/src/app/store/view/view.component.ts
+++ angular-src/src/app/store/view/view.component.ts
@@ -20,7 +20,7 @@ export class ViewComponent implements OnInit {
 
 
     constructor(private StoreService : StoreService) {
-      this.limit = 20 ;
+      this.limit = 5 ;
       this.curPage = 1;
       this.getItemCount();
     }
