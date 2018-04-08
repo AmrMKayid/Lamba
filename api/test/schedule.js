2f1a1e3c99e17da68e28afea0ee6de29d43e4100
Mohamed Helal <m.h.helal1997@gmail.com>
2018-04-08 20:10:09 +0200

    merged with dev branch

---
+++ api/test/schedule.js
@@ -0,0 +1,38 @@
+process.env.NODE_ENV = 'test';
+
+var mongoose = require("mongoose"),
+     User = require('../models/user.model'),
+    Task = require('../models/task.model'),
+    Item = require('../models/item.model'),
+    Article = require('../models/article.model'),
+         route = require('../routes/index'),
+    chai = require('chai'),
+chaiHttp = require('chai-http'),
+    should = chai.should();
+
+describe('Getting schedule',() => {
+    it('should return the teacher schedule',(done) => {
+        var user =  {
+            email: "mm@f.com",
+           role: "Teacher",
+            password: 'wwww',
+            name:{firstname:'Mariam',
+            lastName:'dessouki'}
+        }
+    user.save((err, user) => {
+        route.post('/schedule/createTeacherSchedule/'+user.id)
+        .end((err, res) => {
+        res.should.have.status(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('saturday');
+    res.body.should.have.property('sunday');
+    res.body.should.have.property('monday');
+   res.body.should.have.property('tuesday');
+    res.body.should.have.property('wednesday');
+    res.body.should.have.property('thursday');
+    res.body.should.have.property('friday');
+    done();
+});
+});
+});
+});
