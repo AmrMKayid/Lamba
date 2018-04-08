8030fe4bb57b4ab35177978aaad90be58d30fa9f
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-08 16:53:34 +0200

    Updated navbar

--- angular-src/src/app/shared/navbar/navbar.component.ts
+++ angular-src/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: Boolean;
+
+  constructor(private auth: AuthService) {
+  }
 
   ngOnInit() {
+
+    if (localStorage.getItem('authentication')) {
+      this.isLoggedIn = true;
+    }
+    else {
+      this.isLoggedIn = false;
+    }
+
   }
 
 }
