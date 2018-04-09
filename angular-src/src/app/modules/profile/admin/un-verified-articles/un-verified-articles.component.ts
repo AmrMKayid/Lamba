9f8a35e176d8431e7c50c5269f586378b436f2b7
yasmeen el3sh <yasmeen.abdelmohsen@student.guc.edu.eg>
2018-04-09 18:57:37 +0200

    modified authentication and article corrected

--- angular-src/src/app/modules/profile/admin/un-verified-articles/un-verified-articles.component.ts
+++ angular-src/src/app/modules/profile/admin/un-verified-articles/un-verified-articles.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Http, Headers } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ToasterService } from 'angular5-toaster';
+//import { ToasterService } from 'angular5-toaster';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
@@ -15,32 +15,35 @@ export class UnVerifiedArticlesComponent implements OnInit {
   constructor(
     private httpClient: HttpClient,
      private http: Http,
-    private router: Router,
-    private toast: ToasterService
+    private router: Router
+   // private toast: ToasterService
   ) { }
 
   ngOnInit() {
-    console.log("entered");
-    let autorization =  { Authorization: localStorage.getItem('authorization') };
+    
+   let autorization =  { Authorization: localStorage.getItem('authentication') };
+    console.log("ok");
      this.httpClient.get('http://localhost:3000/api/user/viewUnverifiedArticles',{headers: autorization})
        .subscribe((res: any) => { this.unVerifiedArticlesList = res.data;
-        
-        this.toast.pop({
+        console.log( res.msg);
+        console.log(res.data);
+       /* this.toast.pop({
           type: 'success',
           title: "Success",
           body: res.msg,
           timeout: 3000
-        });
+        });*/
       }, err => {
-        this.toast.pop({
+        console.log( err.error.msg);
+        /*this.toast.pop({
           type: 'error',
           title: "Error!",
           body: err.error.msg,
           timeout: 3000
-        });
+        });*/
       });    
        
-
+      console.log(this.unVerifiedArticlesList);
   }
   showProduct(articleId){
     console.log(articleId);
