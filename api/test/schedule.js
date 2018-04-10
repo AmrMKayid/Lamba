7393dc9b6dbe59c431b00634341d3ada469801d3
Mohamed Helal <m.h.helal1997@gmail.com>
2018-04-10 23:20:43 +0200

    deleted unnecessary methods in tests

--- api/test/schedule.js
+++ api/test/schedule.js
@@ -11,35 +11,9 @@ var config= require('../config'),
     expect=chai.expect,
     chaiHttp = require('chai-http'),
     should = chai.should,
-    testUtils = require('../test/utils');
 chai.use(chaiHttp);
 
-/*describe('Getting schedule',() => {
-    it('should return the teacher schedule',(done) => {
-        var user =  {
-            email: "mm@f.com",
-           role: "Teacher",
-            password: 'wwww',
-            name:{firstname:'Mariam',
-            lastName:'dessouki'}
-        }
-    user.save((err, user) => {
-        route.post('/schedule/createTeacherSchedule/'+user.id)
-        .end((err, res) => {
-        res.should.have.status(200);
-    res.body.should.be.a('object');
-    res.body.should.have.property('saturday');
-    res.body.should.have.property('sunday');
-    res.body.should.have.property('monday');
-   res.body.should.have.property('tuesday');
-    res.body.should.have.property('wednesday');
-    res.body.should.have.property('thursday');
-    res.body.should.have.property('friday');
-    done();
-});
-});
-});
-});*/
+
 describe("schedule ", () => {
     let  teacher , parent , child, teacherToken, parentToken , childToken , teacherId , childId , parentId ;
 
