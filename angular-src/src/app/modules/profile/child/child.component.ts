4e3b1048a75cf15ba61d2a4f1c0c75dc4502df87
Mahmoud Saleh <mmsmhh@Gmail.com>
2018-04-14 17:50:14 +0200

    adding css

--- angular-src/src/app/modules/profile/child/child.component.ts
+++ angular-src/src/app/modules/profile/child/child.component.ts
@@ -88,5 +88,25 @@ export class ChildComponent implements OnInit {
 
   }
 
+closeResult : string;
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
 }
