6c6e4fafb631d14a01aa770dfbfd8ed79a85f44f
Mariam-Dessouki <mariam_mostafa44@yahoo.com>
2018-04-15 22:02:38 +0200

    added schedule in view profile teacher

--- angular-src/src/app/modules/profile/view-profile/view-teacher/view-teacher.component.ts
+++ angular-src/src/app/modules/profile/view-profile/view-teacher/view-teacher.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { appConfig } from "../../../../app.config";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute,Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-view-teacher',
@@ -13,6 +14,15 @@ export class ViewTeacherComponent implements OnInit {
   userID;
   @Input() user;
 
+  //schedule
+  public sat =[];
+  public sun = [];
+  public mon = [];
+  public tues = [];
+  public wed = [];
+  public thurs = [];
+  public fri = [];
+
   constructor(private route: ActivatedRoute,
     private http: HttpClient) {
   }
@@ -20,7 +30,13 @@ export class ViewTeacherComponent implements OnInit {
 
 
   ngOnInit() {
-
+    this.sat = this.user.schedule.table.saturday;
+  this.sun = this.user.schedule.table.sunday;
+  this.mon = this.user.schedule.table.monday;
+  this.tues = this.user.schedule.table.tuesday;
+  this.wed = this.user.schedule.table.wednesday;
+  this.thurs = this.user.schedule.table.thursday;
+  this.fri = this.user.schedule.table.friday;
   }
 
 }
