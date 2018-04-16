7384b061ea1af0e4aad26fb3a80b3afc412f4a4a
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-16 20:35:49 +0200

    Fix the service imports issues

--- angular-src/src/app/shared/notifications/notifications.component.ts
+++ angular-src/src/app/shared/notifications/notifications.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificationService} from '../../services/notification.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-notifications',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private http: HttpClient,
+               private notificationservice: NotificationService) { }
 
+	notifications : Array<Object>;
   ngOnInit() {
+  
+  	this.notificationservice.getMyNotifications().subscribe((res: any) => {
+  		if(res.err != null)
+  		{
+  			/*generate error*/
+  		}
+  		
+  		this.notifications = res.data;
+ 		console.log(this.notifications);
+  	});
+  
   }
 
 }
