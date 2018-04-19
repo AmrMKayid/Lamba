cbd628a4b89778661cff31d95bab7c1e1c2ac356
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-19 19:01:08 +0200

    Create the authentication at the server

--- api/socket/index.js
+++ api/socket/index.js
@@ -1,53 +1,91 @@
 const Message = require('../models/message.model');
 const jwt = require('jsonwebtoken');
+var socketioJwt = require('socketio-jwt');
 
 // array the will store all the connected users
 var clients = [];
+var socketio; 
+
+
 module.exports = function socket(io) {
 
     console.log('TEST: SOCKET is connected');
 
-    io.on('connection', function (socket) {
-
-        console.log('a user connected with id: ' + socket.id);
-
-        // gets the logged in user id
-        const authorization = req.headers.authorization;
-        const secret = req.app.get('secret');
-        decoded = jwt.verify(authorization, secret);
-        var user_id = decoded.user._id;
-
-		client = {
-            id: user_id,
-            socket: socket
-        };
-
-
-        clients.push(client);
-
-        socket.on('disconnect', function () {
-            for( var i=0, len=clients.length; i<len; ++i )
-            {
-                var c = clients[i];
-
-                if(c.clientId == socket.id)
-                {
-                    clients.splice(i,1);
-                    break;
-                }
-            }
-        });
-
-        socket.on('message', (data) => {
-            // Received Message From Angular Client
-            console.log("Message Received: " + data);
-
-            // Sending the message back to the client
-            io.emit('message', {type: 'new-message', text: data});
-
-            let message = new Message(data.message);
-            Message.addMessage(message, (err, newMsg) => {});
-        });
-
-    });
+    socketio = io;
+    io.on('connection',  socketioJwt.authorize({
+    secret: 'SECRET_KEY',
+    timeout: 7000 // 7 seconds to send the authentication message
+    })).on('authenticated',onNewConnection);
 };
+
+
+
+/********************************************************
+ *                                                      *
+ *                 Private Functions                    *
+ *                                                      *
+ ********************************************************/
+
+ /** socket -> void
+   * Makes sure the user is authenticated and adds him to the connected users list
+   *
+   */
+function onNewConnection(socket)
+{
+
+    console.log("new user is attempting to connect");
+    // close the connection
+    if(!socket.decoded_token)
+    {
+        console.log("Unauthorized");
+        socket.disconnect(true);
+        return;
+    }
+
+
+    client = 
+    {
+        id: socket.decoded_token._id,
+        socket: socket
+    };
+    console.log(client.id);
+
+    // removes the client from the list on disconnection
+    socket.on('disconnect', function () {
+        for( var i=0, len=clients.length; i<len; ++i )
+        {
+            var c = clients[i];
+
+            if(c.clientId == socket.id)
+            {
+                clients.splice(i,1);
+                break;
+            }
+        }
+    });
+
+
+    //handles messages
+    socket.on('message', onMessage);
+}
+
+
+/** Message -> void
+  * recevies a message, stores it in the database and forwards it to the correct destination if any
+  *
+  */
+function onMessage(data)
+{
+    if(!data.decoded_token)
+    {
+        return;
+    }
+    // Received Message From Angular Client
+    console.log("Message Received: " + data);
+
+    // Sending the message back to the client
+    io.emit('message', {type: 'new-message', text: data});
+
+    let message = new Message(data.message);
+    Message.addMessage(message, (err, newMsg) => {});
+}
