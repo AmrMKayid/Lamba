0e6201829a538bb9e691ffc3b81c221f3f70c29f
Abdullah Kady <abdullahkady@gmail.com>
2018-04-19 12:02:38 +0200

    Added remove/add fav activities backend functions

--- api/controllers/favorites.controller.js
+++ api/controllers/favorites.controller.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose'),
   Child = mongoose.model('Child'),
   User = mongoose.model('User'),
   Validations = require('../utils/validations'),
-  Article = mongoose.model('Article');
+  Article = mongoose.model('Article'),
+  Activity = mongoose.model('Activity');
 
 
 module.exports.getFavArticles = function (req, res, next) {
@@ -197,7 +198,7 @@ module.exports.removeFavArticle = function (req, res, next) {
   }
   else {
     let userID = req.decodedToken.user._id;
-    User.findById(id, (err, user) => {
+    User.findById(userID, (err, user) => {
       if (err) {
         return next(err);
       }
@@ -224,12 +225,163 @@ module.exports.removeFavArticle = function (req, res, next) {
 ////////////////////////////////////////////////////ACTIVITES//////////////////////////////////////////////////////////////////
 
 module.exports.getFavActivities = function (req, res, next) {
+
 };
 
 module.exports.addFavActivity = function (req, res, next) {
+
+  let valid =
+    req.params.activityID &&
+    Validations.isObjectId(req.params.activityID);
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg: 'Invalid activity ID passed.',
+      data: null
+    });
+  }
+  let activityID = req.params.activityID;
+  let userID = req.decodedToken.user._id;
+  if (req.decodedToken.user.username) {
+
+    Child.findById(userID, (err, child) => {
+      if (err) {
+        return next(err);
+      }
+      if (!child) {
+        return res.status(404).json({
+          err: null,
+          msg: 'Child not found.',
+          data: null
+        });
+      }
+
+      let favoriteIDs = child.favorites.activities;
+      Activity.findById(activityID, (err, activity) => {
+        if (err) {
+          return next(err);
+        }
+        if (!activity) {
+          return res.status(404).json({
+            err: null,
+            msg: 'Activity not found.',
+            data: null
+          });
+        }
+
+        child.favorites.activities.push(activity._id);
+        child.save(function (err, updatedChild) {
+          if (err) return next(err);
+          return res.status(200).json({
+            err: null,
+            msg: 'Activitiy added to favorites successfully',
+            data: updatedChild
+          });
+        });
+      });
+    });
+  } else {
+    User.findById(userID, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(404).json({
+          err: null,
+          msg: 'User not found.',
+          data: null
+        });
+      }
+
+      Activity.findById(activityID, (err, activity) => {
+        if (err) {
+          return next(err);
+        }
+        if (!activity) {
+          return res.status(404).json({
+            err: null,
+            msg: 'Activity not found.',
+            data: null
+          });
+        }
+
+        user.favorites.activities.push(activity._id);
+        user.save(function (err, updatedUser) {
+          if (err) return next(err);
+          return res.status(200).json({
+            err: null,
+            msg: 'Activity added to favorites successfully',
+            data: updatedUser
+          });
+        });
+      });
+    });
+  }
+
 };
 
 module.exports.removeFavActivity = function (req, res, next) {
+
+  let valid =
+    req.params.activityID &&
+    Validations.isObjectId(req.params.activityID);
+  if (!valid) {
+    return res.status(422).json({
+      err: null,
+      msg: 'Invalid activity ID passed.',
+      data: null
+    });
+  }
+
+  let userID = req.decodedToken.user._id;
+  let activityID = req.params.articleId;
+  if (req.decodedToken.user.username) {
+    Child.findById(userID, (err, child) => {
+      if (err) {
+        return next(err);
+      }
+      if (!child) {
+        return res.status(404).json({
+          err: null,
+          msg: 'Child not found.',
+          data: null
+        });
+      }
+      child.favorites.activities.splice(child.favorites.activities.indexOf(activityID), 1);
+      child.save(function (err, updatedChild) {
+        if (err) return next(err);
+        return res.status(200).json({
+          err: null,
+          msg: 'Activity deleted from favorites successfully',
+          data: updatedChild
+        });
+      });
+    });
+  }
+  else {
+    User.findById(userID, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(404).json({
+          err: null,
+          msg: 'User not found.',
+          data: null
+        });
+      }
+      user.favorites.activities.splice(user.favorites.activities.indexOf(activityID), 1);
+      user.save(function (err, updatedUser) {
+        if (err) return next(err);
+        return res.status(200).json({
+          err: null,
+          msg: 'Activity deleted from favorites successfully',
+          data: updatedUser
+        });
+      });
+    });
+  }
+
 };
 
 ////////////////////////////////////////////////////ITEMS//////////////////////////////////////////////////////////////////////
