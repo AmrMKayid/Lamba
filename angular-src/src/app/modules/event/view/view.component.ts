7c052234ecc910a54c6e63f32a9e21fa2164b6c2
Shawky Hussein <ahmed.shawkyegy@gmail.com>
2018-04-19 16:57:01 +0200

    user can view his children in activities

--- angular-src/src/app/modules/event/view/view.component.ts
+++ angular-src/src/app/modules/event/view/view.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from '../../../services/event.service';
 import {Router} from "@angular/router";
+import {AuthService} from "../../../services/auth.service";
 import * as $ from 'jquery';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-view',
@@ -11,9 +13,13 @@ import * as $ from 'jquery';
 export class ViewComponent implements OnInit {
 
   activitiesCount: number;
-  limit: number; 
+  limit: number;
   curPage: number;
   lastPageNumber: number;
+  closeResult: string;
+
+  user;
+  children :any[];
 
 
   activities: any[]; // Current activities
@@ -21,9 +27,12 @@ export class ViewComponent implements OnInit {
 
 
   constructor(private EventService: EventService,
-              private router: Router) {
+              private router: Router,
+                private auth: AuthService,
+            private modalService: NgbModal) {
     this.limit = 20;
     this.curPage = 1;
+    this.user = this.auth.getCurrentUser();
     this.getActivityCount();
   }
 
@@ -85,5 +94,48 @@ export class ViewComponent implements OnInit {
 
   viewInfo(_id) {
     this.router.navigate(['/event/view/' + _id]);
+  }
+
+  getChildren(activityID)
+  {
+
+  }
+
+  open(content,activityID) {
+    this.EventService.getChildren(this.user).subscribe( (children:any)=>{
+      this.EventService.getActivity(activityID).subscribe( (activity:any)=>{
+
+        activity = activity.data;
+        children = children.data;
+
+        for( var child of children)
+        {
+            if(activity.going_user_id.includes(child._id))
+              child['registered'] = true;
+            else
+              child['registered'] = false;
+        }
+
+        this.children = children;
+        console.log(children);
+
+
+        this.modalService.open(content).result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+      });
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
