1ab05b50b85530a13bd4f527fd415aea82f9e965
Abdullah Kady <abdullahkady@gmail.com>
2018-04-19 20:58:01 +0200

    Added favorite activities to the frontend

--- angular-src/src/app/modules/favorites/fav-activities/fav-activities.component.ts
+++ angular-src/src/app/modules/favorites/fav-activities/fav-activities.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fav-activities',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavActivitiesComponent implements OnInit {
 
-  constructor() { }
+  activities: any[];
+  activitiesInitialized: boolean;
+  IMG_URL = 'http://localhost:3000/api/uploads/articlesThumbnails/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('authentication')
+    })
+  };
+
+  constructor(private http: HttpClient,
+    private router: Router) {
+  }
 
   ngOnInit() {
+    this.activities = [];
+    this.activitiesInitialized = false;
+    this.http.get('http://localhost:3000/api//user/favorites/activities', this.httpOptions)
+      .pipe().subscribe(
+        (res: any) => {
+          this.activities = res.data.reverse();
+          this.activitiesInitialized = true;
+        }, err => {
+          this.router.navigate(['/']);
+          new Noty({
+            type: 'error',
+            text: `Activities could not be retrieved: ${err.error.msg}`,
+            timeout: 3000,
+            progressBar: true
+          }).show();
+        }
+      );
   }
 
 }
