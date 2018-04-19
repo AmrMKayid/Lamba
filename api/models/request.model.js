e1459a63616105d8cb02b83a0b72abfe97e2c3a3
Mohamed Helal <m.h.helal1997@gmail.com>
2018-04-19 16:28:36 +0200

    finished requests backend

---
+++ api/models/request.model.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const RequestSchema = new Schema({
+    requestingParentId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    childId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    recievingTeacherId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true
+    }
+});
+
+mongoose.model('Request', RequestSchema);
