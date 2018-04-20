4af7ab91b8198a137d15a6c1007ad7e26c70dae0
Amr M. Kayid <amrkayid2027@gmail.com>
2018-04-20 10:35:41 +0200

    Solve errors in teacher & items html

--- angular-src/src/app/modules/profile/teacher/teacher.component.ts
+++ angular-src/src/app/modules/profile/teacher/teacher.component.ts
@@ -55,7 +55,7 @@ export class TeacherComponent implements OnInit {
   constructor(private router: Router,
               private http: HttpClient,
               private httpClient: HttpClient,
-              private auth: AuthService,
+              public auth: AuthService,
               private modalService: NgbModal) {
   }
 
