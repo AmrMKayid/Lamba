ebf5b0204aa0dd3002d7bd68bb22c3acfec2b94b
MoghazyCoder <abdelrhman.almoghazy@student.guc.edu.eg>
2018-04-20 14:03:55 +0200

    Removed Update component

--- angular-src/src/app/modules/store/myitems/view/view.component.ts
+++ angular-src/src/app/modules/store/myitems/view/view.component.ts
@@ -7,6 +7,9 @@ import {Router} from '@angular/router';
 import {AuthService} from "../../../../services/auth.service";
 import {NgbModal, ModalDismissReasons, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 
+
+import {ActivatedRoute} from "@angular/router";
+
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
@@ -14,66 +17,66 @@ import {NgbModal, ModalDismissReasons, NgbModalRef} from '@ng-bootstrap/ng-boots
 })
 export class ViewComponent implements OnInit {
 
-  myitems= [];
+  myitems: any[];
   itemId: string;
   name: string;
   price: number;
   current: any;
-  key: string = 'name';
   description: string;
   quantity: number;
   item_type: string;
   item_condition: string;
-  newOrEdit = false;
-  createNew = false;
-  editPressed = false;
-  reverse: boolean = false;
   picture_url: string;
-  Name;
+  itemString: any;
+  item: any;
 
   constructor(private http: Http,
     private router: Router,
     private storeservice: StoreService,
     private auth: AuthService,
-    private modalService: NgbModal) {
-this.getMyItems()
+    private modalService: NgbModal,
+    private route: ActivatedRoute) {
+
+            this.route.data = this.item;
+            this.getMyItems()
 }
 
-  sort(key) {
-    this.key = key;
-    this.reverse = !this.reverse;
-  }
+editItem(item) {
+  this.item = item;
+  var itemId = item._id;
 
-  
+  this.name= this.item.name;
+  this.price = this.item.price;
+  this.description = this.item.description;
+  this.quantity = this.item.quantity;
+  this.item_type = this.item.item_type ;
+  this.item_condition = this.item.item_condition ;
 
-  editItem() {
-    var itemString = localStorage.getItem("Update");
-    var item = JSON.parse(itemString)._id;
 
-    let editedItem = {
-      name: this.name,
-      price: Number(this.price),
-      description: this.description,
-      quantity: Number(this.quantity),
-      item_type: this.item_type,
-      item_condition: this.item_condition,
-      updated_at: Date.now()
-    };
+  let editedItem = {
+    name: this.name,
+    price: Number(this.price),
+    description: this.description,
+    quantity: Number(this.quantity),
+    item_type: this.item_type,
+    item_condition: this.item_condition,
+    updated_at: Date.now()
+  };
 
-    this.http.patch('http://localhost:3000/api/store/edit/' + item, editedItem)
-      .subscribe(res => {
-        new Noty({
-          type: 'success',
-          text: 'Updated!',
-          timeout: 3000,
-          progressBar: true
-        }).show();
+  this.http.patch('http://localhost:3000/api/store/edit/' + itemId, editedItem)
+    .subscribe(res => {
+      new Noty({
+        type: 'success',
+        text: 'Updated!',
+        timeout: 3000,
+        progressBar: true
+      }).show();
 
-        localStorage.setItem("Update", null);
-        this.router.navigate(["/store/myitems/view"]);
-      });
+      localStorage.setItem("Update", null);
+      this.router.navigate(["/store/myitems/view"]);
+    });
 
-  }
+}
 
   close() {
 
@@ -81,7 +84,7 @@ this.getMyItems()
     localStorage.setItem("Update", 'null')
 
   }
-  
+
 
 
   getMyItems() {
@@ -110,12 +113,6 @@ this.getMyItems()
 
   }
 
-  update(item) {
-
-    localStorage.setItem("Update", JSON.stringify(item));
-   // this.router.navigate(["/store/myitems/update"]);
-   this.editItem();
-  }
 
 
   ngOnInit() {
