74e310f6689588c5218a0a4f71e1263d4351e39f
Mohamed Helal <m.h.helal1997@gmail.com>
2018-04-20 11:55:01 +0200

    added accept and reject requests in nav bar

--- angular-src/src/app/shared/navbar/navbar.component.ts
+++ angular-src/src/app/shared/navbar/navbar.component.ts
@@ -100,4 +100,75 @@ export class NavbarComponent implements OnInit {
       }
     );
   }
+
+  rejectRequest(request){
+
+    this.httpClient.delete('http://localhost:3000/api/request/deleteRequest/'+request._id,this.httpOptions).subscribe(
+      (res: any) => {
+        this.requests.splice(this.requests.indexOf(request),1);
+      },
+      err => {
+        new Noty({
+          type: 'warning',
+          text: `can not accept request: ${err.error.msg}`,
+          timeout: 5000,
+          progressBar: true
+        }).show();
+      }
+    );
+
+    var notifyParent = {
+      title: "Your request is rejected",
+      description: this.auth.getCurrentUser().name.firstName+" "+this.auth.getCurrentUser().name.lastName+
+      " rejected your request to add your child "+request.childId.name.firstName+" to his/her students",
+      url: "fgasf/sadfs",// todo teacher account
+      recieving_user_id: request.requestingParentId._id
+    };
+    this.notificationservice.CreateNotification(notifyParent);
+  }
+
+  
+  acceptRequest(request){
+    this.httpClient.post('http://localhost:3000/api/user/addStudent/'+request.childId._id ,null,this.httpOptions).subscribe(
+      (res: any) => {
+        new Noty({
+          type: 'success',
+          text: `added child to successfully your students `,
+          timeout: 3000,
+          progressBar: true
+        }).show();
+      }, err => {
+        new Noty({
+          type: 'warning',
+          text: `can not accept request: ${err.error.msg}`,
+          timeout: 5000,
+          progressBar: true
+        }).show();
+      }
+    );
+
+    this.httpClient.delete('http://localhost:3000/api/request/deleteRequest/'+request._id,this.httpOptions).subscribe(
+      (res: any) => {
+        this.requests.splice(this.requests.indexOf(request),1);
+      },
+      err => {
+        new Noty({
+          type: 'warning',
+          text: `can not accept request: ${err.error.msg}`,
+          timeout: 5000,
+          progressBar: true
+        }).show();
+      }
+    );
+
+    var notifyParent = {
+      title: "Your request is accepted",
+      description: this.auth.getCurrentUser().name.firstName+" "+this.auth.getCurrentUser().name.lastName+
+      " accepted your request to add your child "+request.childId.name.firstName+" to his/her students",
+      url: "fgasf/sadfs",// todo teacher account
+      recieving_user_id: request.requestingParentId._id
+    };
+    this.notificationservice.CreateNotification(notifyParent);
+  }
+
 }
