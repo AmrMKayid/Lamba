cd7c54441981bdadbf1b73018fadb2fe415a0055
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-23 13:14:44 +0200

    Create test for the get notification

--- test/notification.js
+++ test/notification.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test';
 
-require('../api/models/item.model');
+require('../api/models/notification.model');
 let mongoose = require("mongoose");
 let Notification = mongoose.model('Notification');
 let jwt = require('jsonwebtoken');
@@ -37,11 +37,15 @@ const auth_token = jwt.sign({
 
 /*Deletes all the items in the data before each test*/
 before(function (done) {
-    Item.remove({}, (err) => {
+    Notification.remove({}, (err) => {
         done();
     });
 });
 
+
+/*call the tests*/
+CreateNotification();
+ViewNotification();
 
 
 
@@ -53,14 +57,23 @@ before(function (done) {
 function CreateNotification()
 {
 	describe('Create multiple Notifications', function () {
-        it('It should create multiple Items', createItemCat);
+        it('It should create multiple Items', createMultipleNotifications);
     });
 
     describe('Notification with missing field', function () {
-        it('It should respond with an error message', createMultipleItems);
+        it('It should respond with an error message', createItemFail);
     });
 }
 
+/*
+ * view Notifications test
+ */
+function ViewNotification()
+{
+	describe('Retrieve Notification', function () {
+        it('Gets all the notifications', viewNotification);
+    });
+}
 
 
 
@@ -81,18 +94,21 @@ function createMultipleNotifications(done)
 	const notification1 = {
 		title: "Mohsen invited you",
 		description: "you was invited by mohsen to an activity",
-		url: "/home"	
+		url: "/home",	
+		recieving_user_id: user._id	
 	};
 
 	const notification2 = {
 		title: "Mohsen messaged you",
 		description: "you send you a message",
-		url: "/home"	
+		url: "/home",
+		recieving_user_id: user._id		
 	};
 	const notification3 = {
 		title: "Mohsen likes you",
 		description: "you was invited by mohsen to an activity",
-		url: "/home"	
+		url: "/home",
+		recieving_user_id: user._id		
 	};
 
 	 chai.request(server).post('/api/notifications/create').set('authorization', auth_token).send(notification1)
@@ -104,6 +120,7 @@ function createMultipleNotifications(done)
                         .end((err, res) => {
 				 Notification.find({}, function (err, docs) {
 
+
                                 docs.should.have.property('length').eql(3);
 
                                 actual_item = docs[0];
@@ -111,14 +128,70 @@ function createMultipleNotifications(done)
                                 actual_item = docs[1];
                                 assertItem(notification2, actual_item);
                                 actual_item = docs[2];
-                                assertItem(notificaion3, actual_item);
-                                done();
+								console.log(docs.length);
+                                assertItem(notification3, actual_item);
+							   Notification.remove({}, (err) => {
+									done();
+								});
                   });
 
 			});
 		});
 
 	});
+
+}
+
+
+/** done -> void
+ * Sends a post request with an item missing a field and checks if the server rejects this item
+ * Function done: should be called when the test is done
+ */
+function createItemFail(done) {
+
+		const notification1 = {
+		title: "Mohsen invited you",
+		description: "you was invited by mohsen to an activity",
+		url: "/home",
+		};
+
+    chai.request(server).post('/api/notifications/create').set('authorization', auth_token).send(notification1)
+        .end((err, res) => {
+
+            // checks for the formate of the response
+            res.should.have.status(422);
+            res.body.should.have.property('msg').eql('One or More field(s) is missing or of incorrect type');
+            res.body.should.have.property('data').should.be.a('object');
+
+            done();
+        });
+}
+
+
+function viewNotification(done)
+{
+		const notification1 = {
+		title: "Mohsen invited you",
+		description: "you was invited by mohsen to an activity",
+		url: "/home",
+		recieving_user_id: user._id		
+		};
+	
+		 chai.request(server).post('/api/notifications/create').set('authorization', auth_token).send(notification1)
+        .end((err, res) => {
+			
+			 chai.request(server).get('/api/notifications/get').set('authorization', auth_token).end((err, res) =>{
+					
+					var data = res.body.data;
+
+					data.should.have.property('length').eql(1);
+					data.recieving_user_id = data.recieving_user_id;
+					assertItem(notification1, data[0]);
+					done();
+		
+			});
+		});
+		
 
 }
 
@@ -133,5 +206,5 @@ function assertItem(expected, actual) {
     actual.should.have.property('title').eql(expected.title);
     actual.should.have.property('description').eql(expected.description);
     actual.should.have.property('url').eql(expected.url);
-    actual.should.have.property('recieving_user_id').eql(user._id);
+
 }
