ad30fed9b7adf7d779d6f9551fe6a3346b137493
MayarAlfares <35994808+MayarAlfares@users.noreply.github.com>
2018-04-23 08:57:53 +0200

    Update view.component.ts

--- angular-src/src/app/modules/event/myactivities/view/view.component.ts
+++ angular-src/src/app/modules/event/myactivities/view/view.component.ts
@@ -5,6 +5,9 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
 import {EventService} from '../../../../services/event.service';
 import {Router} from '@angular/router';
 import {appConfig} from "../../../../app.config";
+import {AuthService} from "../../../../services/auth.service";
+import {NgbModal, ModalDismissReasons, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-view',
@@ -15,11 +18,33 @@ export class ViewComponent implements OnInit {
 
   apiUrlHTML = appConfig.apiUrl;
 
-  myactivities: any;
+  //myactivities: any;
+  activity: any;
+  myactivities = [];
+  activityId: string;
+  name: string;
+  price: number;
+  current: any;
+  key: string = 'name';
+  description: string;
+  place: string;
+  activity_type: string;
+  newOrEdit = false;
+  createNew = false;
+  editPressed = false;
+  reverse: boolean = false;
+  picture_url: string;
 
+  sort(key) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
   constructor(private http: Http,
               private router: Router,
-              private eventservice: EventService,) {
+              private eventservice: EventService,
+              private auth: AuthService,
+              private modalService: NgbModal,
+              private route: ActivatedRoute) {
     this.getMyActivities()
   }
 
@@ -50,14 +75,79 @@ export class ViewComponent implements OnInit {
 
   }
 
-  updateActivity(activity) {
 
-    localStorage.setItem("Update", JSON.stringify(activity));
-    this.router.navigate(["/event/myactivities/update"]);
+  editActivity(activity) {
+    this.activity = activity;
+    var activityId = activity._id;
+
+    this.name = this.activity.name;
+    this.price = this.activity.price;
+    this.description = this.activity.description;
+    this.place = this.activity.place;
+    this.activity_type = this.activity.activity_type;
+
+
+    let editedActivity = {
+      name: this.name,
+      price: Number(this.price),
+      description: this.description,
+      place: this.place,
+      activity_type: this.activity_type,
+      updated_at: Date.now()
+    };
+
+    this.http.patch(appConfig.apiUrl + '/event/edit/' + activityId, editedActivity)
+      .subscribe(res => {
+        new Noty({
+          type: 'success',
+          text: 'Updated!',
+          timeout: 3000,
+          progressBar: true
+        }).show();
+
+        localStorage.setItem("Update", null);
+        this.router.navigate(["/store/myitems/view"]);
+      });
+
   }
 
 
   ngOnInit() {
   }
 
+  close() {
+    
+        this.router.navigate(["/event/myactivities/view"]);
+        localStorage.setItem("Update", 'null')
+    
+      }
+
+      viewInfo(_id) {
+        this.router.navigate(['/event/view/' + _id]);
+      }
+    
+    
+      modalref: NgbModalRef;
+      closeResult: string;
+    
+    
+      open(content) {
+        this.modalref = this.modalService.open(content)
+    
+        this.modalref.result.then((result) => {
+          this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+      }
+    
+      private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+          return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+          return 'by clicking on a backdrop';
+        } else {
+          return `with: ${reason}`;
+        }
+      }
 }
