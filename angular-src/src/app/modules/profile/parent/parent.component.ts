1b399775f9b9c6fe6af47f98b93327e20aeb4580
Mahmoud Saleh <mmsmhh@Gmail.com>
2018-04-25 19:07:01 +0200

    fix logout

--- angular-src/src/app/modules/profile/parent/parent.component.ts
+++ angular-src/src/app/modules/profile/parent/parent.component.ts
@@ -7,6 +7,9 @@ import { AuthService } from "../../../services/auth.service";
 import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { enable, destroy } from 'splash-screen';
+
+
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
@@ -40,6 +43,7 @@ export class ParentComponent implements OnInit {
   }
 
   ngOnInit() {
+
 
     this.user = this.auth.getCurrentUser();
 
