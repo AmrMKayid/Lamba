e32b7f0ea17a62b4c53f2ba455143c8003679860
Abdullah Kady <abdullahkady@gmail.com>
2018-04-29 01:08:50 +0200

    Added the reset component to the frontend

--- angular-src/src/app/shared/reset-password/reset-password.component.ts
+++ angular-src/src/app/shared/reset-password/reset-password.component.ts
@@ -18,8 +18,15 @@ export class ResetPasswordComponent implements OnInit {
 
   ngOnInit() {
     //Holds the token sent by the mail
+    let uuidREGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
     this.resetToken = this.route.snapshot.params['token'];
-    if (!this.resetToken) {
+    if (!this.resetToken || !uuidREGEX.test(this.resetToken)) {
+      new Noty({
+        type: 'error',
+        text: 'Invalid token format, please request a new reset link to your email',
+        timeout: 3500,
+        progressBar: true
+      }).show();
       this.router.navigate(['/']);
     }
   }
@@ -50,10 +57,40 @@ export class ResetPasswordComponent implements OnInit {
       //TODO: Start logic here depending on the backend endpoint
       this.http.post(appConfig.apiUrl + '/user/reset/' + this.resetToken, { password: this.password }).subscribe(
         (res: any) => {
-
+          new Noty({
+            type: 'success',
+            text: `Your password has been changed successfully. Please use your new password to log in`,
+            timeout: 4000,
+            progressBar: true
+          }).show();
+          this.router.navigate(['/login']);
         },
         (error) => {
-
+          let errMsg = error.error ? error.error.msg : error.msg;
+          if (errMsg === 'Invalid token.') {
+            new Noty({
+              type: 'error',
+              text: `Invalid reset link, please try to re-request a new reset email.`,
+              timeout: 4000,
+              progressBar: true
+            }).show();
+            this.router.navigate(['/forget']);
+          } else if (errMsg === 'Token expired.') {
+            new Noty({
+              type: 'warning',
+              text: `Reset link has expired, please request a new reset mail`,
+              timeout: 4000,
+              progressBar: true
+            }).show();
+            this.router.navigate(['/forget']);
+          } else {
+            new Noty({
+              type: 'error',
+              text: `Something went wrong: ${errMsg}`,
+              timeout: 4000,
+              progressBar: true
+            }).show();
+          }
         }
       )
     }
